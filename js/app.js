8b8f16da216abfe2568228617b4b5cffba868508
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2021-11-02 18:49:48 -0500

    feat: Add a Back to Top Button

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ const Section2 = document.querySelector('#section2');
 const Section3 = document.querySelector('#section3');
 const Section4 = document.querySelector('#section4');
 
+const BackUp = document.querySelector('#back-up');
+const PageTop = document.querySelector('#up')
 /**
  * End Global Variables
  * Start Helper Functions
@@ -50,7 +52,7 @@ document.addEventListener('scroll', AddActive);
 function ScrollSection(listener, scroll2){
     listener.addEventListener('click', function(evt){
         evt.preventDefault();
-        scroll2.scrollIntoView({block: "start", behavior: "smooth"});
+        scroll2.scrollIntoView({block: "nearest", behavior: "smooth"});
     });
 }
 
@@ -66,3 +68,15 @@ ScrollSection(s1, Section1);
 ScrollSection(s2, Section2);
 ScrollSection(s3, Section3);
 ScrollSection(s4, Section4);
+// Scroll back up button
+document.addEventListener('scroll', function(evt){
+    if (window.scrollY > 1000){
+        BackUp.style.display = "block";
+    } else {
+        BackUp.style.display = "none";
+    }
+});
+BackUp.addEventListener('click', function(evt){
+    evt.preventDefault();
+    PageTop.scrollIntoView({block: "start", behavior: "smooth"});
+});
