edb32c0d684abe5f122ae4d623b2c81ceb9cd213
Nicolás Albornoz <nico.albornoz.s@gmail.com>
2021-11-02 18:58:19 -0500

    feat: Change the scroll behavior

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ document.addEventListener('scroll', AddActive);
 function ScrollSection(listener, scroll2){
     listener.addEventListener('click', function(evt){
         evt.preventDefault();
-        scroll2.scrollIntoView({block: "nearest", behavior: "smooth"});
+        scroll2.scrollIntoView({block: "start", behavior: "smooth"});
     });
 }
 
@@ -78,5 +78,5 @@ document.addEventListener('scroll', function(evt){
 });
 BackUp.addEventListener('click', function(evt){
     evt.preventDefault();
-    PageTop.scrollIntoView({block: "start", behavior: "smooth"});
+    PageTop.scrollIntoView({block: "center", behavior: "smooth"});
 });
